6d0080df6fefefaa9cb1562892e81dc0e2d782a9
Zadossss <auphanthugo@gmail.com>
2026-01-20 10:27:10 +0100

    Update animated-background.tsx

--- src/components/animated-background.tsx
+++ src/components/animated-background.tsx
@@ -434,7 +434,7 @@ const AnimatedBackground = () => {
           setSplineApp(app);
           bypassLoading();
         }}
-        scene="/assets/skills-keyboard2.spline"
+        scene="/assets/skills-keyboard.spline"
       />
     </Suspense>
   );
