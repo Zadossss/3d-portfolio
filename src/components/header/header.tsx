b1b2f85380b35e202ab1e0fbe958e2ade001cfbd
Zadossss <auphanthugo@gmail.com>
2026-02-02 19:48:35 +0100

    Update header.tsx

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import Link from "next/link";
@@ -16,7 +17,8 @@ interface HeaderProps {
 }
 
 const Header = ({ loader }: HeaderProps) => {
-  const [isActive, setIsActive] = useState<boolean>(false);
+  const [isActive, setIsActive] = useState(false);
+
   return (
     <motion.header
       className={cn(
@@ -25,30 +27,17 @@ const Header = ({ loader }: HeaderProps) => {
       )}
       style={{
         background: isActive ? "hsl(var(--background) / .8)" : "transparent",
-        // backgroundImage:
-        //   "linear-gradient(0deg, rgba(0, 0, 0, 0), rgb(0, 0, 0))",
       }}
-      initial={{
-        y: -80,
-      }}
-      animate={{
-        y: 0,
-      }}
+      initial={{ y: -80 }}
+      animate={{ y: 0 }}
       transition={{
-        delay: loader ? 3.5 : 0, // 3.5 for loading, .5 can be added for delay
+        delay: loader ? 3.5 : 0,
         duration: 0.8,
       }}
     >
-      {/* <div
-        className="absolute inset-0 "
-        style={{
-          mask: "linear-gradient(rgb(0, 0, 0) 0%, rgba(0, 0, 0, 0) 12.5%)",
-        }}
-      >
-      </div> */}
       <div className={cn(styles.bar, "flex items-center justify-between")}>
         <Link href="/" className="flex items-center justify-center">
-          <Button variant={"link"} className="text-md">
+          <Button variant="link" className="text-md">
             {config.author}
           </Button>
         </Link>
@@ -57,36 +46,35 @@ const Header = ({ loader }: HeaderProps) => {
         <OnlineUsers />
 
         <Button
-          variant={"ghost"}
-          onClick={() => setIsActive(!isActive)}
+          variant="ghost"
+          onClick={() => setIsActive((v) => !v)}
           className={cn(
             styles.el,
             "m-0 p-0 h-6 bg-transparent flex items-center justify-center"
           )}
         >
           <div className="relative hidden md:flex items-center">
-            <motion.p
-              variants={opacity}
-              animate={!isActive ? "ouvrir" : "fermer"}
-            >
+            <motion.p variants={opacity} animate={!isActive ? "ouvrir" : "fermer"}>
               Menu
             </motion.p>
             <motion.p variants={opacity} animate={isActive ? "ouvrir" : "fermer"}>
-              Close
+              Fermer
             </motion.p>
           </div>
+
           <div
-            className={`${styles.burger} ${isActive ? styles.burgerActive : ""
-              }`}
-          ></div>
+            className={`${styles.burger} ${isActive ? styles.burgerActive : ""}`}
+          />
         </Button>
       </div>
+
       <motion.div
         variants={background}
         initial="initial"
         animate={isActive ? "ouvrir" : "fermer"}
         className={styles.background}
-      ></motion.div>
+      />
+
       <AnimatePresence mode="wait">
         {isActive && <Nav setIsActive={setIsActive} />}
       </AnimatePresence>
