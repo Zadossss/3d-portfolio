7848e777950a482206f12a6e9a952bc65d238c8f
Zadossss <auphanthugo@gmail.com>
2026-03-11 22:35:45 +0100

    Update projects.tsx

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -172,7 +172,7 @@ const projects: Project[] = [
     id: "codingducks",
     category: "Jeux vidéo Mobile",
     title: "Chick'em",
-    src: "/assets/projects-screenshots/codingducks/poulet1.jpg",
+    src: "/assets/projects-screenshots/codingducks/poulet2.jpg",
     screenshots: ["8.jpg"],
     skills: {
       frontend: [
