ce76e11a3e94df72a6f1ee823069a455252aa5c9
Zadossss <auphanthugo@gmail.com>
2026-03-11 23:52:26 +0100

    Update footer.tsx

--- src/components/header/nav/footer/footer.tsx
+++ src/components/header/nav/footer/footer.tsx
@@ -39,25 +39,6 @@ export default function Footer() {
           <span>Images:</span> Unsplash
         </motion.li>
       </ul>
-      <ul>
-        <motion.li
-          custom={[0.3, 0]}
-          variants={translate}
-          initial="initial"
-          animate="enter"
-          exit="exit"
-        >
-          Blog
-        </motion.li>
-        <motion.li
-          custom={[0.3, 0]}
-          variants={translate}
-          initial="initial"
-          animate="enter"
-          exit="exit"
-        >
-        </motion.li>
-      </ul>
     </div>
   );
 }
